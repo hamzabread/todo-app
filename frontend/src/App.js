fa86f30d378f817670a8adf0696c0c3f87e12052
hamzabread <hamzaval2000@gmail.com>
2026-05-14 00:19:29 +0500

    Fixed API variable in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API = 'http://localhost:5000/api/todos';
+const API = '/api/todos';
 
 function App() {
   const [todos, setTodos] = useState([]);
